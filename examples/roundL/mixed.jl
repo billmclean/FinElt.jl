a2a7c21f77935ef7ef2e5b723bef1d0ce0c1cf6a
William McLean <w.mclean@unsw.edu.au>
2015-01-02 15:48:58 +1100

    Adjusted boundary condition for mixed example

--- examples/roundL/mixed.jl
+++ examples/roundL/mixed.jl
@@ -3,15 +3,15 @@
 using FinElt
 using FinElt.PlanarPoisson
 
-h = 0.1
+h = 0.05
 run(`gmsh -2 -clmax $h -o roundL.msh roundL.geo`)
 
 mesh = read_msh_file("roundL.msh")
 essential_bc = [ "Dirichlet" ]
 vp = VariationalProblem(mesh, essential_bc)
 add_bilin_form!(vp,  "Omega",     grad_dot_grad!)
-add_lin_functnl!(vp, "Omega",     source_times_func!,     x->1.0)
-add_lin_functnl!(vp, "NeumannSW", bdry_source_times_func!, x->-0.25)
+add_lin_functnl!(vp, "Omega",     source_times_func!,     x->2.0)
+add_lin_functnl!(vp, "NeumannSW", bdry_source_times_func!, x->-0.75)
 
 A, b = assembled_linear_system(vp)
 
@@ -22,4 +22,4 @@ write_pos_file("roundL.pos") do fid
     save_warp_nodal_scalar_field(u, "u", mesh, fid)
 end
 
-run(`gmsh mixed_roundL.script`)
+run(`gmsh mixed.script`)
